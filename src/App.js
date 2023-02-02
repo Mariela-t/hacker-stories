b085f697f35662809323f27c76fc4f25ac964dc4
Mariela-t <tinoco.mariela01@gmail.com>
2023-02-01 19:41:24 -0800

    added search engine

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 const title = "React";
@@ -6,6 +5,9 @@ function App() {
   return (
     <div className="App">
       <h1>Hello {title}</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
